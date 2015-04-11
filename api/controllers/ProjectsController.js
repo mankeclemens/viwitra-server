28324de62c154b9408a0600ec1be45ee8f965441
Clemens Manke <clemensmanke@Clemenss-MacBook-Pro.local>
2015-04-11 03:07:33 +0200

    added upload to public tmp folder

--- api/controllers/ProjectsController.js
+++ api/controllers/ProjectsController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
+	serverIsReachable: function (req, res) {    	
+    	return res.json({status:200,isOnline:'ViWiTra Server is ready!'});
+  	},
+  	upload: function (req, res) {
+
+  		req.file('file')
+		.upload({
+			dirname: sails.config.appPath + "/.tmp/public/files/",			
+			maxBytes: 10000000
+		}, function whenDone(err, uploadedFiles) {
+		  if (err) return res.negotiate(err);
+		  else 
+		  	// If no files were uploaded, respond with an error.
+		    if (uploadedFiles.length === 0){
+		      return res.badRequest('No file was uploaded');
+		    }
+		    return res.json({status:200,file:uploadedFiles[0].fd});
+		});
+	}	
 };
 
